8ebe9e7288b5ff35e1eb8461a41fa6719bd3ad2a
Fritz Hoste <fritz@redbeavermeida.be>
2013-10-26 20:32:49 +0100

    no message

--- application/views/home.php
+++ application/views/home.php
@@ -1,244 +1,32 @@
-<!doctype html>
+<div class="row">
+   <div class="col col-lg-12" style="">
 
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
+       <div id="main" role="main">
 
-        <title>iMinds</title>
-        <meta name="description" content="We'll keep performing if free drinks and food is provided.">
-        <meta name="author" content="Devs at iMinds">
+          <ul id="tiles">
+            <!-- These are our grid blocks -->
+            <li><img src="http://1.nieuwsbladcdn.be/Assets/Images_Upload/2013/10/26/dc6d4eec-3e4d-11e3-9669-9545efcb0958_web_scale_0.1855469_0.1855469__.jpg.h380.jpg">
 
-        <link rel="stylesheet" href="<?php echo base_url('css/root.css'); ?>">     
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- Bootstrap -->
-        <link href="<?php echo base_url('css/bootstrap.css'); ?>" rel="stylesheet" media="screen">
+                <p style="font-weight:bold;">Hoofd Brusselse daklozenorganisatie strijkt 190.000 euro per jaar op</p>
+                <p style="font-size:11px;padding-right:5px">De Brusselse daklozenorganisatie Samu Social krijgt een kritisch rapport van de Inspectie Financiën. Naast ondoorzichtige geldstromen en een aantal bedenkelijke reisjes, plaatst de Inspectie vooral vraagtekens bij het loon van directrice Pascale Peraita.\nPeraita ontvangt jaarlijks 192.705 euro bruto van de kleine daklozenorganisatie. Ongezien in de liefdadigheidssector. Ter vergelijking: premier Elio Di Rupo heeft een bruto jaarloon van 290.000 euro.\nYvan Mayeur (PS), voorzitter van het Brusselse OCMW én van Samu Social, volgt de Inspectie Financiën niet in haar oordeel. \u2018Het is niet overdreven. Ik vind dit een heel normaal loon\u2019, liet hij gisteren in een telefonische reactie optekenen.</p>
+                <p style="font-weight:300;font-size:11px;">Shared by </p>
+                <p class="tooltip-demo" style="margin-bottom:50px"><img data-toggle="tooltip" title="Fritz Hoste" src="img/profilepic.jpg" style="float:left;width:30px;" />
 
-        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!--[if lt IE 9]>
-          <script src="<?php echo base_url('js/html5shiv.js'); ?>"></script>
-          <script src="<?php echo base_url('js/respond.min.js'); ?>"></script>
-        <![endif]-->
-          <link rel="stylesheet" href="css/reset.css">
-
-          <!-- Global CSS for the page and tiles -->
-          <link rel="stylesheet" href="css/main.css">
-
-           <!-- Specific CSS for the example -->
-  <style>
-    /**
-     * Grid items
-     */
-    #tiles li {
-      -moz-box-sizing: border-box;
-           box-sizing: border-box;
-    }
-
-    #tiles li img {
-      width: 100%;
-      height: auto;
-    }
-  </style>
-
-    </head>
-
-    <body>
-
-        <div class="container ">
-           <div class="row newspaper">
-              <div class="col col-lg-12">
-              <div class="row">
-                  <div class="col col-lg-2">
-                  <strong>Gather news</strong><br />
-                <img src="img/fb.png" height="35px" style="margin-bottom:5px"/>
-                            <img src="img/twitter.png" height="35px" />
-
-               
-                  </div>
-                  <div class="col col-lg-10">
+                </p>
+                <hr style="width:102%;margin-top:20px;margin-bottom:0px;margin-left:-5px" />
 
 
-                    <h1 class="title">The Social Times</h1>
-                    <h2 class="sub">Get updated by your friends</h2>
+                <a href="google.be"><p style="margin-bottom:5px;"><img src="http://2.nieuwsbladcdn.be/extra/static/img/favicon.ico" style="width:15px;float:left"> 
+                    <span style="color:#b3b3b3;padding-left:10px;font-size:10px">nieuwsblad.be</span></p></a>
+                </li>
 
+                <!-- End of grid blocks -->
+            </ul>
 
-
-
-
-
-                </div>
-                </div>
-            <!-- <div class="row">
-                <div class="col col-lg-12 meta" >
-                    <div class="col-lg-4" style="text-align:left;margin-left:-15px;padding-top:10px">
-                        Sources: Facebook, Twitter
-                    </div>
-                    <div class="col-lg-4" style="text-align:center;font-size:20px">
-                     Saturday 26 October 2013
-                 </div>
-                 <div class="col-lg-4" style="text-align:right;padding-top:10px">
-                    € 5
-                 </div>
-             </div>
-         </div> -->
-
-     </div>
-     <div class="row">
-       <div class="col col-lg-12">
-           <div class="big"></div>
-           <div class="small"></div>
-       </div>
-       <div class="col col-lg-12" style="margin-top:10px">
-       <nav class="navbar navbar-default" role="navigation">
-  <!-- Brand and toggle get grouped for better mobile display -->
-  <div class="navbar-header">
-    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-      <span class="sr-only">Toggle navigation</span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-    </button>
-    <a class="navbar-brand" href="#">The Social Times</a>
-  </div>
-
-  <!-- Collect the nav links, forms, and other content for toggling -->
-  <div class="collapse navbar-collapse navbar-ex1-collapse">
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Recent</a></li>
-      <li><a href="#">Music</a></li>
-      <li><a href="#">Travel</a></li>
-      <li><a href="#">Shopping</a></li>
-     
-    </ul>
-
-    <ul class="nav navbar-nav navbar-right">
-     
-      <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Account <b class="caret"></b></a>
-        <ul class="dropdown-menu">
-          <li><a href="#">Login</a></li>
-          <li><a href="#">Logout</a></li>
-
-        </ul>
-      </li>
-    </ul>
-  </div><!-- /.navbar-collapse -->
-</nav>
-</div>
-   </div>
-
-   <div class="row">
- <div class="col col-lg-12" style="">
-    
-         <div id="main" role="main">
-
-      <ul id="tiles">
-        <!-- These are our grid blocks -->
-        <li><img src="http://1.nieuwsbladcdn.be/Assets/Images_Upload/2013/10/26/dc6d4eec-3e4d-11e3-9669-9545efcb0958_web_scale_0.1855469_0.1855469__.jpg.h380.jpg">
-
-            <p style="font-weight:bold;">Hoofd Brusselse daklozenorganisatie strijkt 190.000 euro per jaar op</p>
-            <p style="font-size:11px;padding-right:5px">De Brusselse daklozenorganisatie Samu Social krijgt een kritisch rapport van de Inspectie Financiën. Naast ondoorzichtige geldstromen en een aantal bedenkelijke reisjes, plaatst de Inspectie vooral vraagtekens bij het loon van directrice Pascale Peraita.\nPeraita ontvangt jaarlijks 192.705 euro bruto van de kleine daklozenorganisatie. Ongezien in de liefdadigheidssector. Ter vergelijking: premier Elio Di Rupo heeft een bruto jaarloon van 290.000 euro.\nYvan Mayeur (PS), voorzitter van het Brusselse OCMW én van Samu Social, volgt de Inspectie Financiën niet in haar oordeel. \u2018Het is niet overdreven. Ik vind dit een heel normaal loon\u2019, liet hij gisteren in een telefonische reactie optekenen.</p>
-            <p style="font-weight:300;font-size:11px;">Shared by </p>
-            <p class="tooltip-demo" style="margin-bottom:50px"><img data-toggle="tooltip" title="Fritz Hoste" src="img/profilepic.jpg" style="float:left;width:30px;" />
-            
-            </p>
-            <hr style="width:102%;margin-top:20px;margin-bottom:0px;margin-left:-5px" />
-
-
-            <a href="google.be"><p style="margin-bottom:5px;"><img src="http://2.nieuwsbladcdn.be/extra/static/img/favicon.ico" style="width:15px;float:left"> 
-                <span style="color:#b3b3b3;padding-left:10px;font-size:10px">nieuwsblad.be</span></p></a>
-        </li>
-
-         <li><img src="http://1.nieuwsbladcdn.be/Assets/Images_Upload/2013/10/26/dc6d4eec-3e4d-11e3-9669-9545efcb0958_web_scale_0.1855469_0.1855469__.jpg.h380.jpg">
-        <p>
-        <img src="http://2.nieuwsbladcdn.be/extra/static/img/favicon.ico" style="width:15px;float:left"> <span style="color:#b3b3b3;padding-left:10px;font-size:10px">nieuwsblad.be</p></li>
-
-         <li><img src="http://1.nieuwsbladcdn.be/Assets/Images_Upload/2013/10/26/dc6d4eec-3e4d-11e3-9669-9545efcb0958_web_scale_0.1855469_0.1855469__.jpg.h380.jpg">
-        <p>
-        <img src="http://2.nieuwsbladcdn.be/extra/static/img/favicon.ico" style="width:15px;float:left"> <span style="color:#b3b3b3;padding-left:10px;font-size:10px">nieuwsblad.be</p></li>
-
-         <li><img src="http://1.nieuwsbladcdn.be/Assets/Images_Upload/2013/10/26/dc6d4eec-3e4d-11e3-9669-9545efcb0958_web_scale_0.1855469_0.1855469__.jpg.h380.jpg">
-        <p>
-        <img src="http://2.nieuwsbladcdn.be/extra/static/img/favicon.ico" style="width:15px;float:left"> <span style="color:#b3b3b3;padding-left:10px;font-size:10px">nieuwsblad.be</p></li>
-
-         <li><img src="http://1.nieuwsbladcdn.be/Assets/Images_Upload/2013/10/26/dc6d4eec-3e4d-11e3-9669-9545efcb0958_web_scale_0.1855469_0.1855469__.jpg.h380.jpg">
-        <p>
-        <img src="http://2.nieuwsbladcdn.be/extra/static/img/favicon.ico" style="width:15px;float:left"> <span style="color:#b3b3b3;padding-left:10px;font-size:10px">nieuwsblad.be</p></li>
-
-         <li><img src="http://1.nieuwsbladcdn.be/Assets/Images_Upload/2013/10/26/dc6d4eec-3e4d-11e3-9669-9545efcb0958_web_scale_0.1855469_0.1855469__.jpg.h380.jpg">
-        <p>
-        <img src="http://2.nieuwsbladcdn.be/extra/static/img/favicon.ico" style="width:15px;float:left"> <span style="color:#b3b3b3;padding-left:10px;font-size:10px">nieuwsblad.be</p></li>
-
-         <li><img src="http://1.nieuwsbladcdn.be/Assets/Images_Upload/2013/10/26/dc6d4eec-3e4d-11e3-9669-9545efcb0958_web_scale_0.1855469_0.1855469__.jpg.h380.jpg">
-        <p>
-        <img src="http://2.nieuwsbladcdn.be/extra/static/img/favicon.ico" style="width:15px;float:left"> <span style="color:#b3b3b3;padding-left:10px;font-size:10px">nieuwsblad.be</p></li>
-
-         <li><img src="http://1.nieuwsbladcdn.be/Assets/Images_Upload/2013/10/26/dc6d4eec-3e4d-11e3-9669-9545efcb0958_web_scale_0.1855469_0.1855469__.jpg.h380.jpg">
-        <p>
-        <img src="http://2.nieuwsbladcdn.be/extra/static/img/favicon.ico" style="width:15px;float:left"> <span style="color:#b3b3b3;padding-left:10px;font-size:10px">nieuwsblad.be</p></li>
-         <li><img src="http://1.nieuwsbladcdn.be/Assets/Images_Upload/2013/10/26/dc6d4eec-3e4d-11e3-9669-9545efcb0958_web_scale_0.1855469_0.1855469__.jpg.h380.jpg">
-        <p>
-        <img src="http://2.nieuwsbladcdn.be/extra/static/img/favicon.ico" style="width:15px;float:left"> <span style="color:#b3b3b3;padding-left:10px;font-size:10px">nieuwsblad.be</p></li>
-
-         <li><img src="http://1.nieuwsbladcdn.be/Assets/Images_Upload/2013/10/26/dc6d4eec-3e4d-11e3-9669-9545efcb0958_web_scale_0.1855469_0.1855469__.jpg.h380.jpg">
-        <p>
-        <img src="http://2.nieuwsbladcdn.be/extra/static/img/favicon.ico" style="width:15px;float:left"> <span style="color:#b3b3b3;padding-left:10px;font-size:10px">nieuwsblad.be</p></li>
-
-        <!-- End of grid blocks -->
-      </ul>
+        </div>
 
     </div>
 
- </div>
-
 
 </div>
 
-</div>
-
-
-</div>
- <!-- include jQuery -->
-  <script src="libs/jquery.min.js"></script>
-
-  <!-- Include the imagesLoaded plug-in -->
-  <script src="libs/jquery.imagesloaded.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-
-  <!-- Include the plug-in -->
-  <script src="jquery.wookmark.js"></script>
-
-  <!-- Once the images are loaded, initalize the Wookmark plug-in. -->
-  <script type="text/javascript">
-    (function ($){
-      $('#tiles').imagesLoaded(function() {
-        // Prepare layout options.
-        var options = {
-          itemWidth: 200, // Optional min width of a grid item
-          autoResize: true, // This will auto-update the layout when the browser window is resized.
-          container: $('#tiles'), // Optional, used for some extra CSS styling
-          offset: 5, // Optional, the distance between grid items
-          outerOffset: 20, // Optional the distance from grid to parent
-          flexibleWidth: 500 // Optional, the maximum width of a grid item
-        };
-
-        // Get a reference to your grid items.
-        var handler = $('#tiles li');
-
-        // Call the layout function.
-        handler.wookmark(options);
-      });
-
-       $('.tooltip-demo').tooltip({
-      selector: "[data-toggle=tooltip]",
-      container: "body"
-    });
-
-    })(jQuery);
-
-       
-  </script>
-
-
-        <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.0.7/angular.min.js"></script>
-    </body>
-</html>
